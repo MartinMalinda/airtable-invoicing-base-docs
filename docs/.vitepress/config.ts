2a7bbaf4cb6768c2ba554e6323f75b61da6bd735
Martin Malinda <malindacz@gmail.com>
2025-07-09 19:29:57 +0200

    add social links

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -67,7 +67,18 @@ export default withMermaid(
           },
         ],
       },
-      socialLinks: [{ icon: "github", link: "https://github.com/your/repo" }],
+      socialLinks: [
+        {
+          icon: "linkedin",
+          link: "https://www.linkedin.com/in/martin-malinda-58b03253/",
+        },
+        {
+          icon: {
+            svg: `<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>`,
+          },
+          link: "https://www.martinmalinda.cz/",
+        },
+      ],
     },
   }),
 );
